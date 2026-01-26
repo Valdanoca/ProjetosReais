a25070f27d6bf53d26c566c5854e3154d3f41f17
Valdanoca <valdanoca98@gmail.com>
2026-01-26 12:08:10 -0300

    atualização para converter reais em dolar

--- script.js
+++ script.js
@@ -8,15 +8,19 @@ const btn = document.querySelector(".btn");
 
 btn.addEventListener("click",()=>{
 const valor = document.querySelector(".valor-entrada").value;
+
 const valor_moeda = document.querySelector(".valor-moeda");
+const valor2_moeda_dolar = document.querySelector(".valor_moeda_dolar");
+
+const convers = 5.28;
+const covertido = valor/convers;
 
 valor_moeda.innerHTML = valor;
-
-
+valor2_moeda_dolar.innerHTML = covertido;
 
 
 
 // console.log(`Botão Clicado com Sucesso!", ${valor.value}`);
-console.log(`Botão Clicado com Sucesso!", ${valor}`);
+    console.log(`Botão Clicado com Sucesso!", ${valor}`);
 
 });
