efe2c7176fdf71772451d7e33212b6d492ed20e1
Valdanoca <valdanoca98@gmail.com>
2026-01-28 03:57:57 -0300

    Todas moedas ververtidas funcionando coretamente

--- script.js
+++ script.js
@@ -10,25 +10,50 @@ btn.addEventListener("click",()=>{
 const valor = document.querySelector(".valor-entrada").value;
 
 const valor_moeda = document.querySelector(".valor-moeda");
-const valor2_moeda_dolar = document.querySelector(".valor_moeda_dolar");
+const valor_moeda_dolar = document.querySelector(".valor_moeda_dolar");
 
-const convers = 5.28;
-const dolar = 6.2;
-const covertido = valor/convers;
+const dolarHoje = 5.2;
+const euroHoje = 6.2;
+const fcfaHoje = 0.0095;
+
+
+const currencys = document.querySelector(".currencys").value;
+
+if(currencys === "dolar"){
+  // USANDO FUNÇÃO DE NUMBER FORMAT PARA FORMATAR VALOR EM DOLAR
+    const covertido1 = valor/dolarHoje;
+    valor_moeda_dolar.innerHTML = new Intl.NumberFormat("ne-US", {
+    style: "currency",
+    currency: "USD"
+    }).format(covertido1);
+   }
+
+if(currencys === "euro"){
+// USANDO FUNÇÃO DE NUMBER FORMAT PARA FORMATAR VALOR EM DOLAR
+    const covertido2 = valor/euroHoje;
+    valor_moeda_dolar.innerHTML = new Intl.NumberFormat("de-DE", {
+    style: "currency",
+    currency: "EUR"
+    }).format(covertido2);
+    }
+
+ if(currencys === "fcfa"){
+ // USANDO FUNÇÃO DE NUMBER FORMAT PARA FORMATAR VALOR EM DOLAR
+    const covertido3 = valor/fcfaHoje;
+    valor_moeda_dolar.innerHTML = new Intl.NumberFormat("fr-FR", {
+     style: "currency",
+    currency: "XOF"
+    }).format(covertido3);
+    }
+
 
 // USANDO FUNÇÃO DE NUMBER FORMAT PARA FORMATAR VALOR EM REAIS
-valor_moeda.innerHTML = new Intl.NumberFormat("pt-BR", {
-style: "currency",
-currency: "BR"
-}).format(valor);
-
-// USANDO FUNÇÃO DE NUMBER FORMAT PARA FORMATAR VALOR EM DOLAR
-valor2_moeda_dolar.innerHTML = new Intl.NumberFormat("ne-US", {
-style: "currency",
-currency: "US"
-}).format(covertido);
- 
+    const covertido2 = valor/euroHoje;
+    valor_moeda.innerHTML = new Intl.NumberFormat("pt-BR", {
+    style: "currency",
+    currency: "BRL"
+    }).format(valor);
+   
 // console.log(`Botão Clicado com Sucesso!", ${valor.value}`);
     console.log(`Botão Clicado com Sucesso!", ${valor}`);
-
 });
